2dbc055a1d3b16aaced6cf0c392ccad8a513819e
Laroon <tetsuyahirao1119@gmail.com>
2021-02-23 20:17:55 +0900

    Add: Sample Test Code.

---
+++ discord-test.py
@@ -0,0 +1,32 @@
+import discord
+import random
+client = discord.Client()
+
+random_contents = [
+    "にゃーん",
+    "わん！",
+    "コケッコッコー",
+    "お嬢",
+    "みら姉"
+]
+
+@client.event
+async def on_ready():
+    print("on_ready")
+    print(discord.__version__)
+
+@client.event
+async def on_message(message):
+    # 送信者がbotである場合は弾く
+    if message.author.bot:
+        return 
+    # メッセージの本文が 鳴いて だった場合
+    if message.content == "鳴いて":
+        # 送信するメッセージをランダムで決める
+        content = random.choice(random_contents)
+        # メッセージが送られてきたチャンネルに送る
+        await message.channel.send(content)
+    elif message.content == "おはよう":
+        await message.channel.send("おはよう！！")
+
+client.run("")
